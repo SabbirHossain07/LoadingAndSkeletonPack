068f78f824bc8ac1ecad254ac80548ae5d0a59e6
Sabbir Hossain <sabbirsopnil07@gmail.com>
2025-12-18 14:29:42 +0600

    Initial Commit

---
+++ LoadingAndSkeletonPack/LoadingAndSkeletonPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingAndSkeletonPackApp.swift
+//  LoadingAndSkeletonPack
+//
+//  Created by Sopnil Sohan on 18/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LoadingAndSkeletonPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
